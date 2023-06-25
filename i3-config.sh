f61a469a45da15db0e679ef1c8f01774bc96504f
cizordj <32869222+cizordj@users.noreply.github.com>
2023-06-24 21:28:00 -0300

    Add a new keybinding

--- i3-config.sh
+++ i3-config.sh
@@ -89,6 +89,7 @@ bindsym \$mod+Ctrl+Left workspace prev
 
 # shows the dmenu password store
 bindsym \$mod+g exec --no-startup-id passmenu -l 10
+bindsym \$mod+t exec --no-startup-id passmenu --type -l 10
 
 # workspaces
 set \$ws1 1
